ed7602f695d59f25c7f61526c7d7c0f24a83fdea
Mathew <dataonlygreater@gmail.com>
2017-08-10 12:12:35 +0100

    Update dtocean-logistics version

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(name='dtocean-installation',
                                                'config/*.xlsx']
                     },
       install_requires=[
-          'dtocean-logistics==1.1.dev0',
+          'dtocean-logistics==1.1.dev1',
           'numpy',
           'pandas',
           'polite>=0.9'
